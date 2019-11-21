72b9c7f82fd277cdaee1ecb467c4ed87d9eb27c3
Erik Granlund <kireerik2008@hotmail.com>
2019-11-21 20:52:35 +0100

    changed the design of the search page and added the next page as well

---
+++ foodsmart/foodsmart/RecipeResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeResultViewController.swift
+//  foodsmart
+//
+//  Created by Erik Granlund on 2019-11-21.
+//  Copyright © 2019 JUDevTeam. All rights reserved.
+//
+
+import UIKit
+import Foundation
+class RecipeResultViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var searchArray:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }	
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
