1de251a6b200d73364a952bc7ef4b1afc728d101
Dennis Karlsson <dennis.karlsson96@outlook.com>
2019-11-26 18:01:15 +0100

    Added API.swift file to access data from Spoonacular API

---
+++ foodsmart/foodsmart/APIRequest.swift
@@ -0,0 +1,44 @@
+//
+//  APIRequest.swift
+//  foodsmart
+//
+//  Created by Dennis Karlsson on 2019-11-26.
+//  Copyright © 2019 JUDevTeam. All rights reserved.
+//
+
+import Foundation
+
+let headers = [
+    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "x-rapidapi-key": "e44daac5e0mshc682df24497a89fp1c4513jsn7067934f0b9b"
+]
+
+let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/trivia/random")! as URL,
+                                        cachePolicy: .useProtocolCachePolicy,
+                                    timeoutInterval: 10.0)
+
+
+
+
+request.httpMethod = "GET"
+
+request.allHTTPHeaderFields = headers
+
+
+
+
+let session = URLSession.shared
+let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+    if (error != nil) {
+        print(error)
+    } else {
+        let httpResponse = response as? HTTPURLResponse
+        print(httpResponse)
+        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            print("\(dataString)")
+        }
+        
+    }
+})
+
+dataTask.resume()
