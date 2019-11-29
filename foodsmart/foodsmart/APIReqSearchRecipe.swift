e1f6944aec2f75de39153fb1ba77ad8799c38d4c
Dennis Karlsson <dennis.karlsson96@outlook.com>
2019-11-29 14:27:48 +0100

    Added @escaping so that callback have time to add result w/o sending empty result back

--- foodsmart/foodsmart/APIReqSearchRecipe.swift
+++ foodsmart/foodsmart/APIReqSearchRecipe.swift
@@ -17,16 +17,20 @@ import Foundation
 
 //To all whom it may concern: Use x.instance.getReturn etc to access functions from this class
 class APIRequest {
-    static let instance = APIRequest()
-    var query: String = ""
-    var url: String = ""
-    var testString: String = ""
-    struct BodyReturn {
-        var recipeID: Int = 0
-        var title: String = ""
-        var readyInMinutes: Int = 0
-        
-    }
+ 
+ static let instance = APIRequest()
+
+ var query: String = ""
+ var url: String = ""
+ var testString: String = ""
+ 
+ 
+ struct BodyReturn {
+     var recipeID: Int = 0
+     var title: String = ""
+     var readyInMinutes: Int = 0
+     
+ }
    
     func setURL(_ url: String) -> Void {
         self.url = url
@@ -39,43 +43,41 @@ class APIRequest {
     
     
     
-    func getReturn() -> String {
-    // TODO MAJOR! Change into functions and classes instead or it will not work at all
-    // Expressions are not allowed at top level will occur otherwise
-    // https://www.youtube.com/watch?v=tdxKIPpPDAI
-    //
-    //
+     func getReturn(completed: @escaping(BodyReturn) -> Void?) {
+        // TODO MAJOR!
+        // Expressions are not allowed at top level will occur otherwise
+        // https://www.youtube.com/watch?v=tdxKIPpPDAI
+        //
+        //
 
-    let headers = [
-        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-        "x-rapidapi-key": "e44daac5e0mshc682df24497a89fp1c4513jsn7067934f0b9b" //Don't touch key for now plz
-    ]
-    let resourceRequest:String = "trivia/random"
-    //TODO URl below to a String var to change between diffrent requests??, see what they return and decide
-    let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/\(resourceRequest)")! as URL,
-                                            cachePolicy: .useProtocolCachePolicy,
-                                        timeoutInterval: 10.0)
-    request.httpMethod = "GET"
-        //Dont know why it's yellow but it does'nt seem to matter... ?
-    request.allHTTPHeaderFields = headers
+            let headers = [
+                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                "x-rapidapi-key": "e44daac5e0mshc682df24497a89fp1c4513jsn7067934f0b9b"
+            ]
 
-    let session = URLSession.shared
-                
-        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-        if (error != nil) {
-            print(error)
-        } else {
-            let httpResponse = response as? HTTPURLResponse
-            print(httpResponse) //This is Header being printed
-            //This is the body which is what we need
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("\(dataString)")
-            }
+            let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?number=10&query=burger")! as URL,
+                                                    cachePolicy: .useProtocolCachePolicy,
+                                                timeoutInterval: 10.0)
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
+
+            let session = URLSession.shared
+            var myStruct = BodyReturn()
+            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                if (error != nil) {
+                    print(error)
+                } else {
+                    let httpResponse = response as? HTTPURLResponse
+                    //print(httpResponse)
+                    if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                        //print("\(dataString)")
+                        myStruct.title = dataString
+                        completed(myStruct)
+                    }
+                }
+            })
+
+            dataTask.resume()
         }
-    })
-
-    dataTask.resume()
-        return testString
+        
     }
-    
-}
