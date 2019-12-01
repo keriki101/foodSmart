5875dcf21cab7def7f971656fa6258442155690c
JOnatan Larsson <jonatan_larsson_@hotmail.com>
2019-12-01 12:00:55 +0100

    fridgeCell

---
+++ foodsmart/foodsmart/WhatsInYourFridge/fridgeCell.swift
@@ -0,0 +1,22 @@
+//
+//  fridgeTableViewCell.swift
+//  foodsmart
+//
+//  Created by Debora Johansson on 11/29/19.
+//  Copyright © 2019 JUDevTeam. All rights reserved.
+//
+
+import UIKit
+
+class FridgeCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var fridgeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+  
+
+}
