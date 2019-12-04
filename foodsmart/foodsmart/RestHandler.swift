7965248fd62180e323332bc9f52c591d23045e42
Dennis Karlsson <dennis.karlsson96@outlook.com>
2019-12-04 12:18:23 +0100

    Committed again 4 gran

--- foodsmart/foodsmart/RestHandler.swift
+++ foodsmart/foodsmart/RestHandler.swift
@@ -11,24 +11,24 @@ import Foundation
 class Resthandler {
     
     static func getReturn(completed: (([Recipe]) -> Void)?) {
-    // TODO MAJOR!
-    // Expressions are not allowed at top level will occur otherwise
-    // https://www.youtube.com/watch?v=tdxKIPpPDAI
-    //
-    //
+        // TODO MAJOR!
+        // Expressions are not allowed at top level will occur otherwise
+        // https://www.youtube.com/watch?v=tdxKIPpPDAI
+        //
+        //
         print("inne i funktionen iaf")
-
+        
         let headers = [
             "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
             "x-rapidapi-key": "e44daac5e0mshc682df24497a89fp1c4513jsn7067934f0b9b"
         ]
-
-        let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=pie")! as URL,
-                                                cachePolicy: .useProtocolCachePolicy,
-                                            timeoutInterval: 10.0)
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=burger")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = headers
-
+        
         let session = URLSession.shared
         
         
@@ -49,14 +49,7 @@ class Resthandler {
                     if let object = json as? [String: Any] {
                         print("inne i object json")
                         print(object)
-                        for anItem in object as! [Dictionary<String, AnyObject>] {
-                            let recipeTitle = anItem["title"] as! String
-                            let recipeID = anItem["id"] as! Int
-                            
-                        }
                     }
-                    
-                    
                 }
                 catch (let error){
                     print(error)
@@ -64,9 +57,7 @@ class Resthandler {
                 }
             }
         })
-
         dataTask.resume()
     }
-
 }
 
